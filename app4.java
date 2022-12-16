c17e67d30b8b6607d7733d0b73d7053b3e76285b
drmalik7 <uchmandx2@gmail.com>
2022-12-16 22:51:08 +0000

    my first commit

---
+++ app4.java
@@ -0,0 +1,12 @@
+#!/bin/bash
+#This is a new file for git
+#this is code for variables
+echo Welcome to the Range
+echo username
+read name
+echo enter id number
+read -s id
+echo $name your credentials match our repo
+date
+
+
